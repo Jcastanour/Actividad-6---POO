0eb048280853c6fa2641bf35f1f9391ebce6fe77
jcastanour <jcastanour@unal.edu.co>
2023-06-24 16:15:43 -0500

    EJERCICIO 6

---
+++ src/actividad_6/Actividad_6.java
@@ -0,0 +1,15 @@
+
+package actividad_6;
+
+//@author Juan Pablo
+public class Actividad_6 {
+
+    public static void main(String[] args) {
+        
+         ventana iu = new ventana();
+         iu.setVisible(true);
+         iu.setLocationRelativeTo(null);
+       
+    }
+
+}
